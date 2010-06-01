c2b5570c16362f5ba2d05fbfe7f7b833bf150b1a
Tobias Wulff <tobias-wulff@codeeye.de>
2010-06-01 18:19:55 +1200

    Converted spaces to tabs in test file

--- test/test_connection.py
+++ test/test_connection.py
@@ -4,24 +4,24 @@ from nose.tools import assert_equals, raises, timed, with_setup
 #from mock import patch
 
 def test_connection_tcp():
-    """Creation of a TCP (default) connection object"""
-    c = connection()
-    assert c
+	"""Creation of a TCP (default) connection object"""
+	c = connection()
+	assert c
 
 def test_connection_udp():
-    """Creation of a UDP connection object"""
-    c = connection('UdP')
-    assert c
+	"""Creation of a UDP connection object"""
+	c = connection('UdP')
+	assert c
 
 def test_connection_from_socket():
-    """Creation of a connection object from an existing socket"""
-    import socket
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    c = connection(sock=s)
-    assert c
+	"""Creation of a connection object from an existing socket"""
+	import socket
+	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	c = connection(sock=s)
+	assert c
 
 @raises(AttributeError)
 def test_connection_from_socket_fail():
-    """Creation of connection object with invalid argument raises Exception"""
-    import socket
-    connection(sock=12345)
+	"""Creation of connection object with invalid argument raises Exception"""
+	import socket
+	connection(sock=12345)
