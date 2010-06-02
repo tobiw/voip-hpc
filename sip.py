cf390598d1abcc0866962a8034a1a44669b5f643
Tobias Wulff <tobias-wulff@codeeye.de>
2010-06-02 13:57:16 +1200

    Added more SIP response codes from Twisted

--- sip.py
+++ sip.py
@@ -19,6 +19,11 @@
 # Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #
 ################################################################################
+#
+# Parts of the SIP response codes are taken from the Twisted Core:
+# http://twistedmatrix.com/trac/wiki/TwistedProjects
+#
+################################################################################
 
 from connection import connection
 
@@ -32,6 +37,7 @@ ACCEPTED                    = '202'
 MULTI_CHOICES               = '300'
 MOVED_PERMANENTLY           = '301'
 MOVED_TEMPORARILY           = '302'
+SEE_OTHER					= '303'
 USE_PROXY                   = '305'
 ALT_SERVICE                 = '380'
 BAD_REQUEST                 = '400'
@@ -49,7 +55,10 @@ LENGTH_REQUIRED             = '411'
 ENTITY_TOO_LARGE            = '413'
 URI_TOO_LARGE               = '414'
 UNSUPPORTED_MEDIA           = '415'
+UNSUPPORTED_URI				= '416'
 BAD_EXTENSION               = '420'
+EXTENSION_REQUIRED			= '421'
+INTERVAL_TOO_BRIEF			= '423'
 NOT_AVAILABLE               = '480'
 NO_TRANSACTION              = '481'
 LOOP                        = '482'
@@ -59,12 +68,15 @@ AMBIGUOUS                   = '485'
 BUSY_HERE                   = '486'
 CANCELLED                   = '487'
 NOT_ACCEPTABLE_HERE         = '488'
+REQUEST_PENDING				= '491'
+UNDECIPHERABLE				= '493'
 INTERNAL_ERROR              = '500'
 NOT_IMPLEMENTED             = '501'
 BAD_GATEWAY                 = '502'
 UNAVAILABLE                 = '503'
 GATEWAY_TIMEOUT             = '504'
 SIP_VERSION_NOT_SUPPORTED   = '505'
+MESSAGE_TOO_LARGE			= '513'
 BUSY_EVERYWHERE             = '600'
 DECLINE                     = '603'
 DOES_NOT_EXIST              = '604'
@@ -87,6 +99,7 @@ RESPONSE = {
 	MULTI_CHOICES:              '300 Multiple choices',
 	MOVED_PERMANENTLY:          '301 Moved permanently',
 	MOVED_TEMPORARILY:          '302 Moved temporarily',
+	SEE_OTHER:					'303 See other',
 	USE_PROXY:                  '305 Use proxy',
 	ALT_SERVICE:                '380 Alternative service',
 
@@ -106,7 +119,10 @@ RESPONSE = {
 	ENTITY_TOO_LARGE:           '413 Request entity too large',
 	URI_TOO_LARGE:              '414 Request-URI too large',
 	UNSUPPORTED_MEDIA:          '415 Unsupported media type',
+	UNSUPPORTED_URI:			'416 Unsupported URI scheme',
 	BAD_EXTENSION:              '420 Bad extension',
+	EXTENSION_REQUIRED:			'421 Extension required',
+	INTERVAL_TOO_BRIEF:			'423 Interval too brief',
 	NOT_AVAILABLE:              '480 Temporarily not available',
 	NO_TRANSACTION:             '481 Call leg/transaction does not exist',
 	LOOP:                       '482 Loop detected',
@@ -116,6 +132,8 @@ RESPONSE = {
 	BUSY_HERE:                  '486 Busy here',
 	CANCELLED:                  '487 Request cancelled',
 	NOT_ACCEPTABLE_HERE:        '488 Not acceptable here',
+	REQUEST_PENDING:			'491 Request pending',
+	UNDECIPHERABLE:				'493 Undecipherable',
 
 	# 5xx
 	INTERNAL_ERROR:             '500 Internal server error',
@@ -124,6 +142,7 @@ RESPONSE = {
 	UNAVAILABLE:                '503 Service unavailable',
 	GATEWAY_TIMEOUT:            '504 Gateway time-out',
 	SIP_VERSION_NOT_SUPPORTED:  '505 SIP version not supported',
+	MESSAGE_TOO_LARGE:			'513 Message too large',
 
 	# 6xx
 	BUSY_EVERYWHERE:            '600 Busy everywhere',
