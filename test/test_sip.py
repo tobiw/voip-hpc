8a5d378b90a8629c15bf06b3d8bbd5e284ed79d3
Tobias Wulff <tobias-wulff@codeeye.de>
2010-06-03 17:15:11 +1200

    Added test cases for SIP connection and SIP message parsing

--- test/test_sip.py
+++ test/test_sip.py
@@ -25,10 +25,81 @@
 #
 ################################################################################
 
-from sip import Sip
+import socket, time
+from sip import sip, parseSipMessage
 from nose.tools import assert_equals, raises, timed, with_setup
 
-def test_sip_default():
-	"""Creation of a default SIP connection object"""
-	s = Sip()
-	assert s
+def test_correct_parsing():
+	"""Test message parsing for correctness"""
+	msgType, headers, body = parseSipMessage("""INVITE sip:foo SIP/2.0
+		From: test
+		To: foo
+		Content-Length: 4
+
+		1234""")
+
+	assert_equals(msgType, "INVITE")
+	assert_equals(headers["from"], "test")
+	assert_equals(headers["to"], "foo")
+	assert_equals(headers["content-length"], "4")
+	assert_equals(body, "1234")
+
+def test_correct_parsing_short_headers():
+	"""Test message parsing for correctness with short headers"""
+	msgType, headers, body = parseSipMessage("""INVITE sip:foo SIP/2.0
+		f: test
+		t: foo
+		l: 4
+
+		1234""")
+
+	assert_equals(msgType, "INVITE")
+	assert_equals(headers["from"], "test")
+	assert_equals(headers["to"], "foo")
+	assert_equals(headers["content-length"], "4")
+	assert_equals(body, "1234")
+
+class TestMessageParser:
+	@classmethod
+	def setUpClass(self):
+		# Create socket to send messages to the SIP parser
+		self.sender = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		self.sender.bind(('', 0))
+
+		# Create SIP parser
+		self.sip = sip()
+
+	@classmethod
+	def tearDownClass(self):
+		self.sender.close()
+
+	def sendMessage(self, msg):
+		self.sender.sendto(msg.encode('utf-8'), (('localhost', 1111)))
+
+	def test_request_with_random_newlines(self):
+		"""Test message parsing with random CRLFs"""
+		self.sendMessage("\r\n\r\n\r\r\n\r\r\r" + """\
+			INVITE sip:foo SIP/2.0
+			From: test
+			To: foo
+			Content-Length: 4
+
+			1234""")
+
+	def test_short_request_with_random_newlines(self):
+		"""Test message parsing with short headers and random CRLFs"""
+		self.sendMessage("\r\n\r\n\r\r\n\r\r\r" + """\
+			INVITE sip:foo SIP/2.0
+			f: test
+			t: foo
+			l: 4
+
+			1234""")
+
+	def test_request_without_contentlength(self):
+		"""Test message parsing without Content-Length"""
+		self.sendMessage("""INVITE sip:foo SIP/2.0
+			f: test
+			t: foo
+
+			123456789""")
